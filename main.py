b75dc0e936c26f54a0eaa6f4a48a47a6fe2ee948
joaomarcelofa <joaov7.1996@gmail.com>
2018-08-15 19:53:31 -0300

    Adicionado funcionalidade de exportar dados para uma tabela excel.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import lxml.html
 from selenium import webdriver
 from lxml import etree
+import xlsxwriter
 
 def process_data(data):
     list_len: int = len(data)
@@ -44,13 +45,36 @@ def format(question):
             new_alternatives = new_alternatives[:-1]
 
     return dict({
-        'questao':question['question'],
+        'questao': question['question'],
         'opcao1': new_alternatives[0],
         'opcao2': new_alternatives[1],
         'opcao3': new_alternatives[2],
         'opcao4': new_alternatives[3],
         'opcaoCorreta': new_alternatives[index_of_correct_alternative]
     })
+
+def save_in_excel(questions):
+    workbook = xlsxwriter.Workbook('teste.xlsx')
+    worksheet = workbook.add_worksheet()
+
+    worksheet.write(0, 0, 'questao')
+    worksheet.write(0, 1, 'opcao1')
+    worksheet.write(0, 2, 'opcao2')
+    worksheet.write(0, 3, 'opcao3')
+    worksheet.write(0, 4, 'opcao4')
+    worksheet.write(0, 5, 'opcaoCorreta')
+
+    row = 1
+    for question in questions:
+        worksheet.write(row, 0, question['questao'])
+        worksheet.write(row, 1, question['opcao1'])
+        worksheet.write(row, 2, question['opcao2'])
+        worksheet.write(row, 3, question['opcao3'])
+        worksheet.write(row, 4, question['opcao4'])
+        worksheet.write(row, 5, question['opcaoCorreta'])
+        row += 1
+
+    workbook.close()
 
 #Open WebDriver
 driver = webdriver.Firefox(executable_path='./geckodriver/geckodriver.exe')
@@ -61,9 +85,7 @@ driver.close()
 etree.strip_tags(tree, 'br')
 
 tableIterator = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
-questions = {}
-
-
+questions = []
 
 for iterator in tableIterator:
     current_xpath = f'/html/body/table/tbody/tr/td[1]/table/tbody/tr[8]/td/table/tbody/tr[{iterator}]/td[2]/font/font/text()'
@@ -77,7 +99,6 @@ for iterator in tableIterator:
 
     question = process_data(current_question)
     if question is not None:
-        questions[iterator] = format(question)
+        questions.append(format(question))
 
-print("HI")
-
+save_in_excel(questions)
---
+++ url.py
@@ -0,0 +1,22 @@
+class urls:
+    def geturls(self):
+        return [
+            'http://www.professor.bio.br/ingles/provas_questoes.asp?section=Adjetivos',
+            'http://www.professor.bio.br/ingles/provas_questoes.asp?section=Adjetivos&curpage=2',
+            'http://www.professor.bio.br/ingles/provas_questoes.asp?section=Adjetivos&curpage=3',
+            'http://www.professor.bio.br/ingles/provas_questoes.asp?section=Adjetivos&curpage=4',
+            'http://www.professor.bio.br/ingles/provas_questoes.asp?section=Adjetivos&curpage=5',
+            'http://www.professor.bio.br/ingles/provas_questoes.asp?section=Adjetivos&curpage=6',
+            'http://www.professor.bio.br/ingles/provas_questoes.asp?section=Adjetivos&curpage=7',
+            'http://www.professor.bio.br/ingles/provas_questoes.asp?section=Adjetivos&curpage=8',
+            'http://www.professor.bio.br/ingles/provas_questoes.asp?section=Adjetivos&curpage=9',
+            'http://www.professor.bio.br/ingles/provas_questoes.asp?section=Adjetivos&curpage=10',
+            'http://www.professor.bio.br/ingles/provas_questoes.asp?section=Adjetivos&curpage=11',
+            'http://www.professor.bio.br/ingles/provas_questoes.asp?section=Adjetivos&curpage=12',
+            'http://www.professor.bio.br/ingles/provas_questoes.asp?section=Adjetivos&curpage=13',
+            'http://www.professor.bio.br/ingles/provas_questoes.asp?section=Adjetivos&curpage=14',
+            'http://www.professor.bio.br/ingles/provas_questoes.asp?section=Adjetivos&curpage=15',
+            'http://www.professor.bio.br/ingles/provas_questoes.asp?section=Adjetivos&curpage=16',
+            'http://www.professor.bio.br/ingles/provas_questoes.asp?section=Adjetivos&curpage=17',
+            'http://www.professor.bio.br/ingles/provas_questoes.asp?section=Adjetivos&curpage=18'
+        ]
