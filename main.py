46d14b478b893432afd5a64ce1bb42ad1ce75311
joaomarcelofa <joaov7.1996@gmail.com>
2018-08-19 13:08:44 -0300

    Adicionado função que avança até a última página automaticamente

--- main.py
+++ main.py
@@ -1,11 +1,67 @@
 import lxml.html
+import xlsxwriter
+from url import urls
 from selenium import webdriver
 from lxml import etree
-import xlsxwriter
+
+
+def crawl(root_url, driver):
+    row_index = 1
+    xpath_first_page = "/html/body/table/tbody/tr/td[1]/table/tbody/tr[11]/td/div/a"
+    x_path_other_pages = "/html/body/table/tbody/tr/td[1]/table/tbody/tr[11]/td/div/b/font/a"
+    workbook = xlsxwriter.Workbook('teste.xlsx')
+    worksheet = workbook.add_worksheet()
+    put_excel_headers(worksheet)
+
+    driver.get(root_url)
+    try:
+        element = driver.find_element_by_xpath(xpath_first_page)
+        tree = lxml.html.fromstring(driver.page_source)
+        current_questions = get_content(tree)
+        save_in_excel(row_index, current_questions, worksheet)
+        row_index += len(current_questions)
+        element.click()
+    except Exception:
+        driver.close()
+        workbook.close()
+
+    while True:
+        try:
+            element = driver.find_element_by_xpath(x_path_other_pages)
+            tree = lxml.html.fromstring(driver.page_source)
+            questions = get_content(tree)
+            save_in_excel(row_index, questions, worksheet)
+            row_index += len(questions)
+            element.click()
+        except Exception:
+            driver.close()
+            workbook.close()
+
+
+def get_content(tree):
+    etree.strip_tags(tree, 'br')
+    tableIterator = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+    questions = []
+
+    for iterator in tableIterator:
+        current_xpath = f'/html/body/table/tbody/tr/td[1]/table/tbody/tr[8]/td/table/tbody/tr[{iterator}]/td[2]/font/font/text()'
+        data_extracted = tree.xpath(current_xpath)
+        current_question = []
+
+        for data in data_extracted:
+            trim_data = data.strip(" ")
+            if ('\n' not in data) and (data is not '') and (len(trim_data) > 0):
+                current_question.append(data.strip())
+
+        question = process_data(current_question)
+        if question is not None:
+            questions.append(format(question))
+    return questions
+
 
 def process_data(data):
     list_len: int = len(data)
-    if list_len == 6 or list_len == 7:
+    if len(data[list_len - 1]) is 3 and (list_len is 6 or list_len is 7):
         question: str = data[0]
         correct_answer = data[(list_len - 1)]
         alternatives = data[1:-1]
@@ -53,10 +109,7 @@ def format(question):
         'opcaoCorreta': new_alternatives[index_of_correct_alternative]
     })
 
-def save_in_excel(questions):
-    workbook = xlsxwriter.Workbook('teste.xlsx')
-    worksheet = workbook.add_worksheet()
-
+def put_excel_headers(worksheet):
     worksheet.write(0, 0, 'questao')
     worksheet.write(0, 1, 'opcao1')
     worksheet.write(0, 2, 'opcao2')
@@ -64,41 +117,17 @@ def save_in_excel(questions):
     worksheet.write(0, 4, 'opcao4')
     worksheet.write(0, 5, 'opcaoCorreta')
 
-    row = 1
+
+def save_in_excel(row_index, questions, worksheet):
     for question in questions:
-        worksheet.write(row, 0, question['questao'])
-        worksheet.write(row, 1, question['opcao1'])
-        worksheet.write(row, 2, question['opcao2'])
-        worksheet.write(row, 3, question['opcao3'])
-        worksheet.write(row, 4, question['opcao4'])
-        worksheet.write(row, 5, question['opcaoCorreta'])
-        row += 1
+        worksheet.write(row_index, 0, question['questao'])
+        worksheet.write(row_index, 1, question['opcao1'])
+        worksheet.write(row_index, 2, question['opcao2'])
+        worksheet.write(row_index, 3, question['opcao3'])
+        worksheet.write(row_index, 4, question['opcao4'])
+        worksheet.write(row_index, 5, question['opcaoCorreta'])
+        row_index += 1
 
-    workbook.close()
-
-#Open WebDriver
-driver = webdriver.Firefox(executable_path='./geckodriver/geckodriver.exe')
-driver.get('http://www.professor.bio.br/ingles/provas_questoes.asp?section=Adjetivos&curpage=18')
-tree = lxml.html.fromstring(driver.page_source)
-driver.close()
-
-etree.strip_tags(tree, 'br')
-
-tableIterator = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
-questions = []
-
-for iterator in tableIterator:
-    current_xpath = f'/html/body/table/tbody/tr/td[1]/table/tbody/tr[8]/td/table/tbody/tr[{iterator}]/td[2]/font/font/text()'
-    data_extracted = tree.xpath(current_xpath)
-    current_question = []
-
-    for data in data_extracted:
-        trim_data = data.strip(" ")
-        if ('\n' not in data) and (data is not '') and (len(trim_data) > 0):
-            current_question.append(data.strip())
-
-    question = process_data(current_question)
-    if question is not None:
-        questions.append(format(question))
-
-save_in_excel(questions)
+driver = webdriver.Firefox(executable_path='./geckodriver/geckodriver.exe') #Open WebDriver
+url_dict = urls().geturls()
+crawl(url_dict["adjetivos"], driver)
--- url.py
+++ url.py
@@ -1,22 +1,15 @@
 class urls:
     def geturls(self):
-        return [
-            'http://www.professor.bio.br/ingles/provas_questoes.asp?section=Adjetivos',
-            'http://www.professor.bio.br/ingles/provas_questoes.asp?section=Adjetivos&curpage=2',
-            'http://www.professor.bio.br/ingles/provas_questoes.asp?section=Adjetivos&curpage=3',
-            'http://www.professor.bio.br/ingles/provas_questoes.asp?section=Adjetivos&curpage=4',
-            'http://www.professor.bio.br/ingles/provas_questoes.asp?section=Adjetivos&curpage=5',
-            'http://www.professor.bio.br/ingles/provas_questoes.asp?section=Adjetivos&curpage=6',
-            'http://www.professor.bio.br/ingles/provas_questoes.asp?section=Adjetivos&curpage=7',
-            'http://www.professor.bio.br/ingles/provas_questoes.asp?section=Adjetivos&curpage=8',
-            'http://www.professor.bio.br/ingles/provas_questoes.asp?section=Adjetivos&curpage=9',
-            'http://www.professor.bio.br/ingles/provas_questoes.asp?section=Adjetivos&curpage=10',
-            'http://www.professor.bio.br/ingles/provas_questoes.asp?section=Adjetivos&curpage=11',
-            'http://www.professor.bio.br/ingles/provas_questoes.asp?section=Adjetivos&curpage=12',
-            'http://www.professor.bio.br/ingles/provas_questoes.asp?section=Adjetivos&curpage=13',
-            'http://www.professor.bio.br/ingles/provas_questoes.asp?section=Adjetivos&curpage=14',
-            'http://www.professor.bio.br/ingles/provas_questoes.asp?section=Adjetivos&curpage=15',
-            'http://www.professor.bio.br/ingles/provas_questoes.asp?section=Adjetivos&curpage=16',
-            'http://www.professor.bio.br/ingles/provas_questoes.asp?section=Adjetivos&curpage=17',
-            'http://www.professor.bio.br/ingles/provas_questoes.asp?section=Adjetivos&curpage=18'
-        ]
+        return {
+            "adjetivos": "http://www.professor.bio.br/ingles/provas_questoes.asp?section=Adjetivos",
+            "adverbios": "http://www.professor.bio.br/ingles/provas_questoes.asp?section=Adverbios",
+            "artigos": "http://www.professor.bio.br/ingles/provas_questoes.asp?section=Artigos",
+            "conjuncao": "http://www.professor.bio.br/ingles/provas_questoes.asp?section=Conjuncao",
+            "construcao-palavra": "http://www.professor.bio.br/ingles/provas_questoes.asp?section=construcao-palavra",
+            "falsoscognatos": "http://www.professor.bio.br/ingles/provas_questoes.asp?section=falsoscognatos",
+            "ingles": "http://www.professor.bio.br/ingles/provas_questoes.asp?section=Ingles",
+            "interpretacao-texto": "http://www.professor.bio.br/ingles/provas_questoes.asp?section=interpretacao-texto",
+            "pronomes": "http://www.professor.bio.br/ingles/provas_questoes.asp?section=Pronomes",
+            "verbos": "http://www.professor.bio.br/ingles/provas_questoes.asp?section=Verbos",
+            "vocabulario": "http://www.professor.bio.br/ingles/provas_questoes.asp?section=vocabulario"
+        }
